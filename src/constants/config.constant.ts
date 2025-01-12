e64e36b019c893824679b4efc25026b4cabfe1dc
qnguyen <nguyenquan.212002@gmail.com>
2025-01-12 17:46:06 +0700

    .

--- src/constants/config.constant.ts
+++ src/constants/config.constant.ts
@@ -1,7 +1,7 @@
 const config = {
-   //baseUrl: 'http://localhost:8080',
+  //  baseUrl: 'http://localhost:8080',
   baseUrl:'https://booking-tour-g3ggeqa5bcfkdwgq.australiasoutheast-01.azurewebsites.net',
-  frontEndUrl: 'https://local-guide-for-travelers.vercel.app/'
+  frontEndUrl: 'https://wb-booking-tour.netlify.app/'
 }
 
 export default config
