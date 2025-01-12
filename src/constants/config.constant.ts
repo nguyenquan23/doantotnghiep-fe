9aa57f4ecb8283243fb54774095b8ee2def0eacd
qnguyen <nguyenquan.212002@gmail.com>
2025-01-12 18:43:58 +0700

    .

--- src/constants/config.constant.ts
+++ src/constants/config.constant.ts
@@ -1,6 +1,6 @@
 const config = {
-    baseUrl: 'http://localhost:8080',
-  //baseUrl:'https://booking-tour-g3ggeqa5bcfkdwgq.australiasoutheast-01.azurewebsites.net',
+   // baseUrl: 'http://localhost:8080',
+  baseUrl:'https://booking-tour-g3ggeqa5bcfkdwgq.australiasoutheast-01.azurewebsites.net',
   frontEndUrl: 'https://wb-booking-tour.netlify.app/'
 }
 
