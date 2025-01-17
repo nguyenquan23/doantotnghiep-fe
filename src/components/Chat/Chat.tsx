daaf7414972d81e4555da654bc47a3259b584ed5
qnguyen <nguyenquan.212002@gmail.com>
2025-01-17 08:11:44 +0700

    .

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -36,7 +36,7 @@ const Chat: React.FC = () => {
     setIsTyping(true);
 
     try {
-      const response = await fetch("https://73af-42-1-99-74.ngrok-free.app/chat", {
+      const response = await fetch("https://ed09-42-1-99-74.ngrok-free.app/chat", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
